6835fdd647f38d59ef9270c748cdde72eb207bf4
한지선 <seon.hannn@gmail.com>
2024-10-19 13:16:42 +0900

    feat: set react

--- board_fe/src/App.js
+++ board_fe/src/App.js
@@ -1,30 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import {Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
-  const [boards, setBoards] = useState([]);
+    const [boards, setBoards] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:8080/api/boards')
-        .then(response => response.json())
-        .then(data => setBoards(data))
-        .catch(error => console.log(error));
-  });
+    useEffect(() => {
+        fetch('http://localhost:8080/api/boards')
+            .then(response => response.json())
+            .then(data => setBoards(data))
+            .catch(error => console.log(error));
+    }, []); // 빈 배열을 추가하여 컴포넌트가 처음 렌더링될 때만 실행되도록 합니다.
 
-  return (
-      <div className="App">
-        <div className={"header"}>게시판</div>
-        <ul>
-          {boards.map((board) => (
-              <li key={board.id}>
-                <div className={"title"}>{board.title}</div>
-                <div className={"writer"}>{board.userId}</div>
-                <div className={"date"}>{board.regDate}</div>
-              </li>
-          ))}
-        </ul>
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className={"header"}>게시판</div>
+            <ul>
+                {boards.map((board) => (
+                    <li key={board.id}>
+                        <Link to={`/${board.id}`}>
+                            <div className={"title"}>{board.title}</div>
+                            <div className={"writer"}>{board.userId}</div>
+                            <div className={"date"}>{new Date(board.regDate).toLocaleDateString()}</div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
--- src/main/java/com/example/board_jpa/controller/BoardController.java
+++ src/main/java/com/example/board_jpa/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.example.board_jpa.controller;
 
+import com.example.board_jpa.model.entity.Board;
 import com.example.board_jpa.model.response.BoardResponse;
 import com.example.board_jpa.service.BoardServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class BoardController {
     List<BoardResponse> findBoards() {
         String boardStatusCode = "ACTIVATE";
         return  boardService.findBoardsByStatusCode(boardStatusCode);
+    }
+
+    @GetMapping("/{id}")
+    Board findBoardById(@PathVariable Long id) {
+        return boardService.findBoardById(id);
     }
 }
